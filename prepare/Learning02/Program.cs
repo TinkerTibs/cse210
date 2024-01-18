0347423a318d78dca09bdf458f922fa77359a194
TinkerTibs <71239220+TinkerTibs@users.noreply.github.com>
2024-01-18 09:23:34 -0600

    W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._company = "Thorin and Company";
+        job1._jobTitle = "Burglar";
+        job1._startYear = 2941;
+        job1._endYear = 2942;
+
+        Job job2 = new Job();
+        job2._company = "Gandalf";
+        job2._jobTitle = "Ring-bearer";
+        job2._startYear = 3018;
+        job2._endYear = 3019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Aragorn Arathornson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
